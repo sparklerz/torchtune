39dc5ec18adb02858e49b2267c3d4c5af666b630
sarat kannan <kannansarat9@gmail.com>
2024-10-13 23:35:57 +0530

    remove unknown flags error

--- torchtune/config/_parse.py
+++ torchtune/config/_parse.py
@@ -49,12 +49,6 @@ class TuneRecipeArgumentParser(argparse.ArgumentParser):
         """
         namespace, unknown_args = super().parse_known_args(*args, **kwargs)
 
-        unknown_flag_args = [arg for arg in unknown_args if arg.startswith("--")]
-        if unknown_flag_args:
-            raise ValueError(
-                f"Additional flag arguments not supported: {unknown_flag_args}. Please use --config or key=value overrides"
-            )
-
         config = OmegaConf.load(namespace.config)
         assert "config" not in config, "Cannot use 'config' within a config file"
         self.set_defaults(**config)
