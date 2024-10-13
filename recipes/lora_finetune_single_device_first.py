092830cb1c93cfc6e422768f82627dffa6d10e76
sarat kannan <kannansarat9@gmail.com>
2024-10-14 03:00:27 +0530

    fix parameter values

--- recipes/lora_finetune_single_device_first.py
+++ recipes/lora_finetune_single_device_first.py
@@ -284,15 +284,15 @@ class LoRAFinetuneRecipeSingleDevice(FTRecipeInterface):
 
             # Wrap the optimizer with Hivemind
             self._optimizer = hivemind.Optimizer(
-                dht=self._dht,
-                run_id=cfg.hivemind.run_id,
-                batch_size_per_step=cfg.batch_size,
-                target_batch_size=cfg.hivemind.target_batch_size,
-                optimizer=self._optimizer,
-                use_local_updates=cfg.hivemind.use_local_updates,
-                matchmaking_time=cfg.hivemind.matchmaking_time,
-                averaging_timeout=cfg.hivemind.averaging_timeout,
-                verbose=cfg.hivemind.verbose
+                dht=self._dht,              # use a DHT that is connected with other peers            
+                run_id='my_llama_run',      # unique identifier of this collaborative run
+                batch_size_per_step=8,      # each call to opt.step adds this many samples towards the next epoch
+                target_batch_size=1000,     # after peers collectively process this many samples, average weights and begin the next epoch
+                optimizer=self._optimizer,  # wrap the SGD optimizer defined above
+                use_local_updates=True,     # perform optimizer steps with local gradients, average parameters in background
+                matchmaking_time=3.0,       # when averaging parameters, gather peers in background for up to this many seconds
+                averaging_timeout=10.0,     # give up on averaging if not successful in this many seconds
+                verbose=True                # print logs incessently
             )
         else:
             log.warning("No host_maddrs provided. DHT and Hivemind optimizer not initialized.")
