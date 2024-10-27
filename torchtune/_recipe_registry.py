698a899ea8f2b5cb43f46709b26245c1464cff1e
sarat kannan <kannansarat9@gmail.com>
2024-10-28 03:08:30 +0530

    add more lora configs

--- torchtune/_recipe_registry.py
+++ torchtune/_recipe_registry.py
@@ -105,9 +105,29 @@ _ALL_RECIPES = [
         supports_distributed=True,
     ),
     Recipe(
-        name="hivemind_qlora_finetune_first_peer",
+        name="hivemind_lora_finetune_first_peer",
         file_path="lora_finetune_single_device_first.py",
         configs=[
+            Config(
+                name="qwen2/0.5B_lora_single_device",
+                file_path="qwen2/0.5B_lora_single_device.yaml",
+            ),
+            Config(
+                name="llama3_2/1B_lora_single_device",
+                file_path="llama3_2/1B_lora_single_device.yaml",
+            ),
+            Config(
+                name="qwen2/1.5B_lora_single_device",
+                file_path="qwen2/1.5B_lora_single_device.yaml",
+            ),
+            Config(
+                name="gemma/2B_lora_single_device",
+                file_path="gemma/2B_lora_single_device.yaml",
+            ),
+            Config(
+                name="llama3_2/3B_lora_single_device",
+                file_path="llama3_2/3B_lora_single_device.yaml",
+            ),
             Config(
                 name="llama2/7B_lora_single_device",
                 file_path="llama2/7B_lora_single_device.yaml",
