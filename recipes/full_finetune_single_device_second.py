207112aa962d0695d61c27edf7e250a2fe661611
sarat kannan <kannansarat9@gmail.com>
2024-11-11 01:46:41 +0530

    increase timeout

--- recipes/full_finetune_single_device_second.py
+++ recipes/full_finetune_single_device_second.py
@@ -267,9 +267,9 @@ class FullFinetuneRecipeSingleDevice(FTRecipeInterface):
                 optimizer=optimizer_lambda,  # wrap the optimizer defined above
                 params=self._model.parameters(),
                 use_local_updates=True,     # perform optimizer steps with local gradients, average parameters in background
-                matchmaking_time=60.0,       # when averaging parameters, gather peers in background for up to this many seconds
-                averaging_timeout=120.0,     # give up on averaging if not successful in this many seconds
-                load_state_timeout=180.0,    # Add explicit timeout for loading state
+                matchmaking_time=120.0,       # when averaging parameters, gather peers in background for up to this many seconds
+                averaging_timeout=180.0,     # give up on averaging if not successful in this many seconds
+                load_state_timeout=300.0,    # Add explicit timeout for loading state
                 offload_optimizer=False,
                 verbose=True,               # print logs incessently
             )
