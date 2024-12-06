a713bdf31d5f52611f20609f6e3fbecbcb206f8b
sarat kannan <kannansarat9@gmail.com>
2024-12-07 01:10:25 +0530

    add sleep time

--- recipes/full_finetune_single_device_second.py
+++ recipes/full_finetune_single_device_second.py
@@ -263,7 +263,7 @@ class FullFinetuneRecipeSingleDevice(FTRecipeInterface):
                 dht=self._dht,              # use a DHT that is connected with other peers            
                 run_id='my_llama_run',      # unique identifier of this collaborative run
                 batch_size_per_step=1,      # each call to opt.step adds this many samples towards the next epoch
-                target_batch_size=100,     # after peers collectively process this many samples, average weights and begin the next epoch
+                target_batch_size=10,       # after peers collectively process this many samples, average weights and begin the next epoch
                 optimizer=optimizer_lambda,  # wrap the optimizer defined above
                 params=self._model.parameters(),
                 use_local_updates=True,     # perform optimizer steps with local gradients, average parameters in background
@@ -801,6 +801,7 @@ class FullFinetuneRecipeSingleDevice(FTRecipeInterface):
                 # Note we are stepping each batch, which might not include optimizer step in the trace
                 # if the schedule cycle doesn't align with gradient accumulation.
                 self._profiler.step()
+                time.sleep(15)
 
             self.epochs_run += 1
             self.save_checkpoint(epoch=curr_epoch)
